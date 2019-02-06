70097326940125e8bbaa0091903fec4fb71a2b0a
Juan Pablo Royo Sales <juanpablo.royo@schibsted.com>
2019-02-06 23:41:26 +0100

    Iterating

--- src/main/scala/program.scala
+++ src/main/scala/program.scala
@@ -2,23 +2,7 @@ package program
 
 import scala.util.{ Random, Success, Try }
 
-
-/**
-  * This is an exercise to explore advantages of moving from imperative design to FP design
-  *
-  * We are going to define program example which is going to take several arguments pased through command line
-  * and evaluate each command line in order to execute diferent branch of the program.
-  *
-  * Story 1: As an user i want to get recommendations from an specific algorith, but if there are no recommendations for this algorith
-  * or i forgot to specify what algorithm should be use i would like to have default recommendations from the best algorithm the system has.
-  *
-  * Story 2: As an user i want to get a message if recommendation's algorithm i requested is wrong.
-  *
-  * Story 3: As an user i want to be able to be retrieve with a limited number of recommendations
-  *
-  */
-object AppImperative {
-
+object DataSource {
   case class UserId(userId: Int) extends AnyVal
   case class Rec(recId: String, score: Float)
   case class UserRec(userId: UserId, recs: List[Rec])
@@ -51,26 +35,52 @@ object AppImperative {
 
   val algoDefault = "algo1"
 
-  def program(args: Array[String]): Unit = {
-    if(args.length < 1){
+}
+
+
+/**
+  * This is an exercise to explore advantages of moving from imperative design to FP design
+  *
+  * We are going to define program example which is going to take several arguments pased through command line
+  * and evaluate each command line in order to execute diferent branch of the program.
+  *
+  * Story 1: As an user i want to get recommendations from an specific algorith, but if there are no recommendations for this algorith
+  * or i forgot to specify what algorithm should be use i would like to have default recommendations from the best algorithm the system has.
+  *
+  * Story 2: As an user i want to get a message if recommendation's algorithm i requested is wrong.
+  *
+  * Story 3: As an user i want to be able to be retrieve with a limited number of recommendations
+  *
+  */
+object AppImperative {
+
+  import DataSource._
+
+  def getCommandLineArgs(args: Array[String]): (String, String, String) = {
+
+    if (args.length < 1) {
       println("At least userId must be provided")
       sys.exit(1)
     }
 
     val userIdArg = args(0)
     var recommenderId = algoDefault
-    if(args.length > 1){
+    if (args.length > 1) {
       recommenderId = args(1)
     }
     var limitArg = ""
-    if(args.length > 2){
+    if (args.length > 2) {
       limitArg = args(2)
     }
+    (userIdArg, recommenderId, limitArg)
+  }
 
-    var userId: Int = -1 
+
+  def convertArgs(userIdArg: String, limitArg: String): (Int, Int) = {
+    var userId: Int = -1
     var limit: Int = 10
 
-    if(userIdArg != null && userIdArg.nonEmpty){
+    if (userIdArg != null && userIdArg.nonEmpty) {
       Try(userIdArg.toInt) match {
         case Success(number) => userId = number
         case _ => {
@@ -78,12 +88,12 @@ object AppImperative {
           sys.exit(1)
         }
       }
-    }else{
+    } else {
       println("UserId must be provided")
       sys.exit(1)
     }
 
-    if(limitArg != null && limitArg.nonEmpty) {
+    if (limitArg != null && limitArg.nonEmpty) {
       Try(limitArg.toInt) match {
         case Success(number) => limit = number
         case _ => {
@@ -92,32 +102,58 @@ object AppImperative {
         }
       }
     }
+    (userId, limit)
+  }
 
-    if(users.exists(_.userId == userId)){
+  private def getRecommendations(request: CreateRequest): Unit = {
+    val userId = request.userId
+    val recommenderId = request.recommenderId
+    val limit = request.limit
+    if (users.exists(_.userId == userId)) {
       var algoId: String = algoDefault
-      if(recommenderId != null && recommenderId.nonEmpty){
-        if(algorithms.keys.exists(_ == recommenderId)){
+      if (recommenderId != null && recommenderId.nonEmpty) {
+        if (algorithms.keys.exists(_ == recommenderId)) {
           algoId = recommenderId
         }
       }
       var result = algorithms.get(algoId).get(userId)
-      if(result.recs.isEmpty){
+      if (result.recs.isEmpty) {
         result = algorithms.get(algoDefault).get(userId)
       }
-      if(result.recs.isEmpty){
+      if (result.recs.isEmpty) {
         println(s"No recommendations found for userId $userId")
-      }else{
+      } else {
         val filteredResult = result.copy(recs = recs.slice(0, limit).toList)
         println(s"\nRecommnedations for userId $userId...")
         println(s"Algorithm $algoId")
         println(s"Recs: ${filteredResult.recs}")
       }
       sys.exit(0)
-    }else{
+    } else {
       println(s"No user found with userId $userId")
       sys.exit(1)
     }
+  }
 
+
+  def program(args: Array[String]): Unit = {
+
+    val request = createRequest(args)
+
+    getRecommendations(request)
+
+  }
+
+  case class CreateRequest(recommenderId: String, userId: Int, limit: Int)
+
+  private def createRequest(args: Array[String]): CreateRequest = {
+    val getCommandLineArgsResult: (String, String, String) = getCommandLineArgs(args)
+    val userIdArg: String = getCommandLineArgsResult._1
+    var recommenderId: String = getCommandLineArgsResult._2
+    var limitArg: String = getCommandLineArgsResult._3
+
+    var (userId: Int, limit: Int) = convertArgs(userIdArg, limitArg)
+    CreateRequest(recommenderId, userId, limit)
   }
 }
 
