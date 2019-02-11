582c1c0d73513a413eadef2adffa0d87232c2d1a
Juan Pablo Royo Sales <juanpablo.royo@schibsted.com>
2019-02-11 17:59:23 +0100

    Improving

--- src/main/scala/program.scala
+++ src/main/scala/program.scala
@@ -103,26 +103,30 @@ object AppImperative {
     val result = for {
       user           <- getUser(userId)
       algorithm      <- getAlgorithm(recommenderId)
-      result         <- algorithm.run(user)
+      result         <- executeAlgorithm(user, algorithm)
       limitFilter     = limit.getOrElse(limitDefault)
       resultFiltered <- filterResults(result, limitFilter)
     } yield Result(algorithm, resultFiltered)
     result
   }
 
+
   def printResults(userId: Option[Int], result: Option[Result]): Unit = {
     result.fold(println(s"No recommendations found for userId $userId"))(algoRes => {
-        println(s"\nRecommnedations for userId ${algoRes.recs.userId}...")
-        println(s"Algorithm ${algoRes.algorithm.name}")
-        println(s"Recs: ${algoRes.recs.recs}")
+      println(s"\nRecommnedations for userId ${algoRes.recs.userId}...")
+      println(s"Algorithm ${algoRes.algorithm.name}")
+      println(s"Recs: ${algoRes.recs.recs}")
     })
   }
 
   private def getUser(userId: Option[Int]): Option[UserId] =
-    userId.filter(user => users.exists(_.userId == user)).map(UserId)
+  userId.filter(user => users.exists(_.userId == user)).map(UserId)
 
   private def getAlgorithm(recommenderId: Option[String]): Option[Algorithm] =
-    recommenderId.orElse(algoDefault).flatMap(algorithms.get(_))
+  recommenderId.orElse(algoDefault).flatMap(algorithms.get(_))
+
+  private def executeAlgorithm(user: UserId, algorithm: Algorithm): Option[UserRec] =
+    algorithm.run(user)
 
   private def filterResults(result: UserRec, limitFilter: Int): Option[UserRec] =
     Some(result.copy(recs = recs.slice(0, limitFilter).toList))
