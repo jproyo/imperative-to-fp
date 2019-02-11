9722f04f8232cfa37596c6591abcdc26b29b3bf9
Juan Pablo Royo Sales <juanpablo.royo@schibsted.com>
2019-02-11 18:07:03 +0100

    Improving

--- src/main/scala/program.scala
+++ src/main/scala/program.scala
@@ -77,14 +77,27 @@ object algebras {
     def getUser(userId: Option[Int]): F[UserId]
   }
 
+
+  object UserRepo {
+    def apply[F[_]](implicit UserR: UserRepo[F]): UserRepo[F] = UserR
+  }
+
+
   trait Filter[F[_]] {
     def filter(userRec: UserRec, limit: Int): F[UserRec]
   }
 
+  object Filter {
+    def apply[F[_]](implicit Fil: Filter[F]): Filter[F] = Fil
+  }
 
   trait AlgorithmRepo[F[_]] {
     def getAlgorithm(recommenderId: Option[String]): F[Algorithm]
     def execute(algo: Algorithm, userId: UserId): F[UserRec]
+  }
+
+  object AlgorithmRepo {
+    def apply[F[_]](implicit Algo: AlgorithmRepo[F]): AlgorithmRepo[F] = Algo
   }
 
 
